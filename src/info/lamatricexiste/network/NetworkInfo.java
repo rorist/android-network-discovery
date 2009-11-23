f86dc3b4d9b8ac630ad006fccd6ccfb1883a73fb
rorist <rorist@wichside.(none)>
2009-11-23 21:52:23 +0100

    release 0.2.4

--- src/info/lamatricexiste/network/NetworkInfo.java
+++ src/info/lamatricexiste/network/NetworkInfo.java
@@ -30,8 +30,8 @@ public class NetworkInfo {
 		int i = dhcp.netmask;
 		i = i - ((i >> 1) & 0x55555555);
 		i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-//		return ((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
-		return 24;
+		return ((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+//		return 24;
 	}
 
 	public InetAddress getIp() {
