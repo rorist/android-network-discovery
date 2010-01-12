93a272f10c6ecc20f972e18a8a8bdee28eb9e733
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2010-01-12 15:31:46 +0100

    Make native from script

--- scripts/generate_native.sh
+++ scripts/generate_native.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 PATH_TO_APP='/home/rorist/workspace/SmbExploit'
 PATH_TO_NDK='/opt/android-ndk'
-APPS_DIR='apps/net'
-cd $PATH_TO_NDK
+APP_NAME='net'
 
-if [ -d "$APPS_DIR" ]; then
-  echo 'exist'
-else
-  mkdir $APPS_DIR
-  ln -s $PATH_TO_APP/jni/Application.mk $APPS_DIR/.
+cd $PATH_TO_NDK
+if [ -d "apps/$APP_NAME"! ]; then
+  mkdir apps/$APP_NAME
+  ln -s $PATH_TO_APP/jni/Application.mk apps/$APP_NAME/.
 fi
+make APP=$APP_NAME -B
+cd -
