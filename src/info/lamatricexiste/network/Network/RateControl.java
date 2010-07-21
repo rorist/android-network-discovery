eb14d060cfd74b8839334a4512b7ed2e4d24e105
Aubort Jean-Baptiste <rorist@gmail.com>
2010-07-21 16:32:00 +0200

    AdaptRate: compile pattern only once, use final variables

--- src/info/lamatricexiste/network/Network/RateControl.java
+++ src/info/lamatricexiste/network/Network/RateControl.java
@@ -6,7 +6,6 @@
 package info.lamatricexiste.network.Network;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.util.regex.Matcher;
@@ -16,63 +15,65 @@ import android.util.Log;
 
 public class RateControl {
 
-    // TODO: Calculate a rounded up value from experiments in different networks
-    private final String TAG = "RateControl";
-    private final int REACH_TIMEOUT = 5000;
-    public String[] indicator;
-    public int rate = 800; // Slow start
-    public boolean is_indicator_discovered = false;
+	// TODO: Calculate a rounded up value from experiments in different networks
+	private final String TAG = "RateControl";
+	private final int REACH_TIMEOUT = 5000;
+	private final String CMD = "/system/bin/ping";
+	private final String ARG = " -q -n -W 2 -c ";
+	private final String PTN = "^rtt min\\/avg\\/max\\/mdev = [0-9\\.]+\\/[0-9\\.]+\\/([0-9\\.]+)\\/[0-9\\.]+ ms$";
+	private Pattern mPattern;
+	public String[] indicator;
+	public int rate = 800; // Slow start
+	public boolean is_indicator_discovered = false;
 
-    public void adaptRate() {
-        int response_time = 0;
-        // TODO: Use an indicator with a port, calculate java round trip time
-        // if (indicator.length > 1) {
-        // Log.v(TAG, "use a socket here, port=" + getIndicator()[1]);
-        // } else {
-        is_indicator_discovered = true;
-        if ((response_time = getAvgResponseTime(indicator[0], 3)) > 0) {
-            if (response_time > 100) { // Most distanced hosts
-                rate = response_time * 5; // Minimum 500ms
-            } else {
-                rate = response_time * 10; // Maximum 1000ms
-            }
-            Log.v(TAG, "adapt=" + response_time + "ms -> " + rate + "ms");
-        }
-        // }
-    }
+	public RateControl() {
+		mPattern = Pattern.compile(PTN);
+	}
 
-    private int getAvgResponseTime(String host, int count) {
-        try {
-            String cmd = "/system/bin/ping";
-            if ((new File(cmd)).exists() == true) {
-                String line;
-                Matcher matcher;
-                Process p = Runtime.getRuntime().exec(cmd + " -q -n -W 2 -c " + count + " " + host);
-                BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()), 1);
-                while ((line = r.readLine()) != null) {
-                    // Log.d("    ", line);
-                    // rtt min/avg/max/mdev = 1.281/2.693/4.939/1.605 ms
-                    matcher = Pattern
-                            .compile(
-                                    "^rtt min\\/avg\\/max\\/mdev = [0-9\\.]+\\/[0-9\\.]+\\/([0-9\\.]+)\\/[0-9\\.]+ ms$")
-                            .matcher(line);
-                    if (matcher.matches()) {
-                        return (int) Float.parseFloat(matcher.group(1));
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "Can't use native ping: " + e.getMessage());
-            try {
-                final long start = System.nanoTime();
-                if (InetAddress.getByName(host).isReachable(REACH_TIMEOUT)) {
-                    Log.i(TAG, "Using Java ICMP request instead ...");
-                    return (int) ((System.nanoTime() - start) / 1000);
-                }
-            } catch (Exception e1) {
-                Log.e(TAG, e1.getMessage());
-            }
-        }
-        return rate;
-    }
+	public void adaptRate() {
+		int response_time = 0;
+		// TODO: Use an indicator with a port, calculate java round trip time
+		// if (indicator.length > 1) {
+		// Log.v(TAG, "use a socket here, port=" + getIndicator()[1]);
+		// } else {
+		is_indicator_discovered = true;
+		if ((response_time = getAvgResponseTime(indicator[0], 3)) > 0) {
+			if (response_time > 100) { // Most distanced hosts
+				rate = response_time * 5; // Minimum 500ms
+			} else {
+				rate = response_time * 10; // Maximum 1000ms
+			}
+			// Log.v(TAG, "adapt=" + response_time + "ms -> " + rate + "ms");
+		}
+		// }
+	}
+
+	private int getAvgResponseTime(String host, int count) {
+		try {
+			// TODO: Reduce allocation
+			String line;
+			Matcher matcher;
+			Process proc = Runtime.getRuntime().exec(CMD + ARG + count + " " + host);
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(proc.getInputStream()), 1);
+			while ((line = reader.readLine()) != null) {
+				matcher = mPattern.matcher(line);
+				if (matcher != null && matcher.matches()) {
+					return (int) Float.parseFloat(matcher.group(1));
+				}
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "Can't use native ping: " + e.getMessage());
+			try {
+				final long start = System.nanoTime();
+				if (InetAddress.getByName(host).isReachable(REACH_TIMEOUT)) {
+					Log.i(TAG, "Using Java ICMP request instead ...");
+					return (int) ((System.nanoTime() - start) / 1000);
+				}
+			} catch (Exception e1) {
+				Log.e(TAG, e1.getMessage());
+			}
+		}
+		return rate;
+	}
 }
