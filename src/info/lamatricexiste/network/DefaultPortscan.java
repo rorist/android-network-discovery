118aa729a1d13643b63ab45a0083163940402c88
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2010-07-28 11:32:51 +0200

    Some perf changes in DefaultPortscan

--- src/info/lamatricexiste/network/DefaultPortscan.java
+++ src/info/lamatricexiste/network/DefaultPortscan.java
@@ -6,7 +6,7 @@
 /**
  * Java NIO Documentation:
  * http://jfarcand.wordpress.com/2006/05/30/tricks-and-tips-with-nio-part-i-why-you-must-handle-op_write
- * http://jfarcand.wordpress.com/2006/06/06/tricks-and-tips-with-nio-part-ii-why-selectionkeyattach-evil
+ * http://jfarcand.wordpress.com/2006/07/06/tricks-and-tips-with-nio-part-ii-why-selectionkey-attach-is-evil/ 
  * http://jfarcand.wordpress.com/2006/07/07/tricks-and-tips-with-nio-part-iii-thread-or-not-thread
  * http://jfarcand.wordpress.com/2006/07/19/tricks-and-tips-with-nio-part-iv-meet-selectors
  * http://jfarcand.wordpress.com/2006/09/21/tricks-and-tips-with-nio-part-v-ssl-and-nio-friend-or-foe
@@ -17,6 +17,7 @@ package info.lamatricexiste.network;
 import info.lamatricexiste.network.Utils.Prefs;
 
 import java.io.IOException;
+import java.lang.StringIndexOutOfBoundsException;
 import java.lang.ref.WeakReference;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -34,7 +35,6 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.util.SparseArray;
 
 public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
 
@@ -42,25 +42,34 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
     private final int MAX_READ = 75;
     private final int TIMEOUT_SELECT = 300; // milliseconds
     private final long TIMEOUT_READ = 1500;
+    private boolean getBanner = false;
     private int cnt_selected;
     private long timeout = 0;
     private long time;
     private Selector connSelector = null;
     private Selector readSelector = null;
 
-    protected WeakReference<Activity> mActivity;
     protected String[] mBanners = null;
     protected String ipAddr = null;
     protected int port_start = 0;
     protected int port_end = 0;
     protected int nb_port = 0;
 
-    protected DefaultPortscan(Activity activity, String host, final int rate) {
-        mActivity = new WeakReference<Activity>(activity);
+    protected DefaultPortscan(Activity activity, String host, int rate) {
+        WeakReference<Activity> mActivity = new WeakReference<Activity>(activity);
+        final Activity d = mActivity.get();
+        if (d != null) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(d.getApplicationContext());
+            getBanner = prefs.getBoolean(Prefs.KEY_BANNER, Prefs.DEFAULT_BANNER);
+        }
         this.ipAddr = host;
+        // Not less than 100ms
+        if (rate < 100) {
+            rate = 100;
+        }
         // From milliseconds to nanoseconds
         this.timeout = (long) (rate * 1000 * 1000);
-        Log.v(TAG, "timeout=" + rate + "ms");
+        Log.i(TAG, "timeout=" + rate + "ms");
     }
 
     protected Void doInBackground(Void... params) {
@@ -71,11 +80,9 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
                 // FIXME: Selector leaks file descriptors (Dalvik bug)
                 // http://code.google.com/p/android/issues/detail?id=4825
                 for (int i = port_start; i <= port_end - step; i += step + 1) {
-                    time = System.nanoTime();
                     start(ina, i, i + ((i + step <= port_end - step) ? step : port_end - i));
                 }
             } else {
-                time = System.nanoTime();
                 start(ina, port_start, port_end);
             }
         } catch (UnknownHostException e) {
@@ -109,6 +116,7 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
             connectSocket(ina, i);
             // Thread.sleep(timeout);
         }
+        time = System.nanoTime();
         doSelect(PORT_END - PORT_START);
     }
 
@@ -149,10 +157,7 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
         SocketChannel socket = SocketChannel.open();
         socket.configureBlocking(false);
         socket.connect(new InetSocketAddress(ina, port));
-        // Register the Channel with port as attachement
-        SparseArray<Integer> data = new SparseArray<Integer>(1);
-        data.append(0, port);
-        socket.register(connSelector, SelectionKey.OP_CONNECT, data);
+        socket.register(connSelector, SelectionKey.OP_CONNECT, new Integer(port));
     }
 
     private void doSelect(final int NB) {
@@ -183,58 +188,54 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
     private void handleConnect(SelectionKey key) {
         try {
             if (((SocketChannel) key.channel()).finishConnect()) { // Open
-                final Activity d = mActivity.get();
-                if (d != null) {
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(d
-                            .getApplicationContext());
-                    if (prefs.getBoolean(Prefs.KEY_BANNER, Prefs.DEFAULT_BANNER)) {
-                        // TODO: Send a Probe before reading !
-                        // SelectionKey tmpKey = ((SocketChannel)
-                        // key.channel()).register(
-                        // readSelector, SelectionKey.OP_WRITE);
-                        // tmpKey.interestOps(tmpKey.interestOps() |
-                        // SelectionKey.OP_WRITE);
-                        // int code = readSelector.select(TIMEOUT_READ);
-                        // tmpKey.interestOps(tmpKey.interestOps() &
-                        // (~SelectionKey.OP_WRITE));
-                        // if (code != 0) {
-                        // ByteBuffer data =
-                        // Charset.forName("ISO-8859-1").encode(
-                        // "GET / HTTP/1.0\r\n\r\n");
-                        // SocketChannel sock = (SocketChannel)
-                        // tmpKey.channel();
-                        // while (data.hasRemaining()) {
-                        // sock.write(data);
-                        // }
-                        // data.clear();
-                        // Log.v(TAG, "writing ...");
-                        // }
-                        // Create a new selector and register for reading
-                        SelectionKey tmpKey = ((SocketChannel) key.channel()).register(
-                                readSelector, SelectionKey.OP_READ);
-                        tmpKey.interestOps(tmpKey.interestOps() | SelectionKey.OP_READ);
-                        int code = readSelector.select(TIMEOUT_READ);
-                        tmpKey.interestOps(tmpKey.interestOps() & (~SelectionKey.OP_READ));
-                        if (code != 0) {
-                            handleRead(tmpKey, ((SparseArray<Integer>) key.attachment()).get(0));
-                            time = System.nanoTime(); // Reset selector timeout
-                            finishKey(key);
-                            return;
-                        }
-                        time = System.nanoTime();
-                        finishKey(tmpKey);
+                if (getBanner) {
+                    // TODO: Send a Probe before reading !
+                    // SelectionKey tmpKey = ((SocketChannel)
+                    // key.channel()).register(
+                    // readSelector, SelectionKey.OP_WRITE);
+                    // tmpKey.interestOps(tmpKey.interestOps() |
+                    // SelectionKey.OP_WRITE);
+                    // int code = readSelector.select(TIMEOUT_READ);
+                    // tmpKey.interestOps(tmpKey.interestOps() &
+                    // (~SelectionKey.OP_WRITE));
+                    // if (code != 0) {
+                    // ByteBuffer data =
+                    // Charset.forName("ISO-8859-1").encode(
+                    // "GET / HTTP/1.0\r\n\r\n");
+                    // SocketChannel sock = (SocketChannel)
+                    // tmpKey.channel();
+                    // while (data.hasRemaining()) {
+                    // sock.write(data);
+                    // }
+                    // data.clear();
+                    // Log.v(TAG, "writing ...");
+                    // }
+
+                    // Register for reading on the read selector
+                    SelectionKey tmpKey = ((SocketChannel) key.channel()).register(
+                            readSelector, SelectionKey.OP_READ);
+                    tmpKey.interestOps(tmpKey.interestOps() | SelectionKey.OP_READ);
+                    int code = readSelector.select(TIMEOUT_READ);
+                    tmpKey.interestOps(tmpKey.interestOps() & (~SelectionKey.OP_READ));
+                    if (code != 0) {
+                        handleRead(tmpKey, (Integer) key.attachment());
+                        time = System.nanoTime(); // Reset selector timeout
+                        finishKey(key);
+                        return;
                     }
+                    time = System.nanoTime();
+                    finishKey(tmpKey);
                 }
-                publishProgress(((SparseArray<Integer>) key.attachment()).get(0), (int) 1);
+                publishProgress((Integer) key.attachment(), (int) 1);
                 finishKey(key);
             }
         } catch (IOException e) { // Closed
-            publishProgress(((SparseArray<Integer>) key.attachment()).get(0), (int) 0);
+            publishProgress((Integer) key.attachment(), (int) 0);
             finishKey(key);
         }
     }
 
-    private void handleRead(SelectionKey key, int port) {
+    private void handleRead(SelectionKey key, final int port) {
         ByteBuffer bbuf = ByteBuffer.allocate(MAX_READ);
         int numRead = 0;
         try {
@@ -244,6 +245,8 @@ public class DefaultPortscan extends AsyncTask<Void, Integer, Void> {
             if (numRead > 8) {
                 mBanners[port] = new String(bbuf.array()).substring(0, numRead).trim();
             }
+        } catch (StringIndexOutOfBoundsException e) {
+            Log.e(TAG, e.getMessage());
         } catch (IOException e) {
             Log.e(TAG, e.getMessage());
         } finally {
